6a6564fb022f870864353c08b57f740bf5f87388
ChenYong <yong.chen@du-power.com>
2019-12-19 16:38:36 +0800

    前十个题目问题代码

---
+++ chenyong-bee-courseBill/chenyong-bee-courseBill-server/src/main/java/Question/question002.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class question002 {
+  public static void main(String[] args) {
+    System.out.println("ssss");
+  }
+  public void methods(){
+    ArrayList<VehicleOne> myList = new ArrayList<>();
+    myList.add(new VehicleOne());
+   }
+}
+
---
+++ chenyong-bee-courseBill/chenyong-bee-courseBill-server/src/main/java/Question/question004.java
@@ -0,0 +1,19 @@
+public class question004 {
+
+  public static void main(String[] args) {
+    Short s1=200;
+
+    Integer s2=400;
+    Long s3=(long) s1+s2;
+    Long s4=(long) (s3*s2);
+
+
+    System.out.println(s3);
+
+   // short a=129;
+   // Byte b=(byte)a;
+   // String s4=(String) (s3*s2);
+//    System.out.println("Sum is"+s4);
+  }
+
+}
